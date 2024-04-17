98d83cc78c6f5c71cf79f0de39371e784b4085fd
coppietersdm <matthieu.coppieters@student.uclouvain.be>
2024-04-17 09:40:39 +0200

    integration of the external forces

--- selfmade/MBS_design.py
+++ selfmade/MBS_design.py
@@ -6,6 +6,13 @@ from MBS_class import *
 
 MBS = mbs()
 
+MBS.add_body(MBS.base,"R2", 
+             dii=np.array([0.0,0.0,-3.0]),    
+             dhi=np.array([0.0,0.0,-2.0]),   
+             q0=np.array([1.0,0,0]),
+             anchor_points = [np.array([0.0,0.0,-4.0])]
+             )
+
 MBS.add_body(MBS.base,      "R2", m=1.0, I=I2, dii=np.array([0.0,0.0,-1.0]),    dhi=np.array([0.0,0.0,-2.0]),   q0=np.array([1.0,0,0]))
 MBS.add_body(MBS.bodies[-1],"T3", m=1.0, I=I2, dii=np.array([0.0,0.0,-1.0]),    dhi=np.array([0.0,0.0,-2.0]),   Q = lambda t,q,qd:-100.0*q)
 MBS.add_body(MBS.bodies[-1],"R2", m=1.0, I=I2, dii=np.array([0.0,0.0,-1.0]),    dhi=np.array([0.0,0.0,0.0]),    q0=np.array([0.0,0.0,0.0]))
